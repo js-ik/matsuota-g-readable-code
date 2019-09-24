73478c9d8442a9b861cf1ba66ab1c74b13b78c17
Tadashi Matsuo <tadashi.matsuo@justsystems.com>
2019-09-24 13:12:50 +0900

    spec3: 処理概要記載

--- sample1/src/sample1/Training.java
+++ sample1/src/sample1/Training.java
@@ -4,6 +4,14 @@ public class Training {
 
 	public static void main(String[] args) {
 		
+		// 引数チェック
+		
+		// 入力ファイル存在チェック
+		
+		// 単語情報読み込み
+		
+		// 単語情報出力
+		
 		System.out.println("上手");
 
 	}
