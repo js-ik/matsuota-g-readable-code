9ae973e533168f0d345db5de4b19c1236f470dae
Tadashi Matsuo <tadashi.matsuo@justsystems.com>
2019-09-24 13:25:23 +0900

    spec3: 引数チェック処理を追加

--- sample1/src/sample1/Training.java
+++ sample1/src/sample1/Training.java
@@ -5,6 +5,9 @@ public class Training {
 	public static void main(String[] args) {
 		
 		// 引数チェック
+		if (!checkArgs(args)) {
+			return;
+		}
 		
 		// 入力ファイル存在チェック
 		
@@ -16,4 +19,18 @@ public class Training {
 
 	}
 
+	// 引数チェック
+	// 引数が1つ以外の場合、エラーメッセージを表示し、チェックエラーとする
+	private static boolean checkArgs(String[] args) {
+
+		// 単語情報ファイルが指定されているか確認する
+		if (args.length !=1) {
+			System.out.println("単語情報ファイルを指定して下さい。");
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
 }
