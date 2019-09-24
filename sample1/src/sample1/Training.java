d33cf4858c4ee625fa4bfd65ef6c7471b77def8d
ishihark <kenji.ishihara@justsystems.com>
2019-09-24 14:52:32 +0900

    spec3: ファイルの読み込み処理と出力処理を記載（どこに追加すればよいかが事前にコメントが記載されていたため明確）

--- sample1/src/sample1/Training.java
+++ sample1/src/sample1/Training.java
@@ -1,56 +1,70 @@
 package sample1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class Training {
 
-	public static void main(String[] args) {
-		
-		// 引数チェック
-		if (!checkArgs(args)) {
-			return;
-		}
-		
-		// 単語情報ファイル存在チェック
-		if (!existDictionaryDataFile(args[0])) {
-			return;
-		}
-		
-		// 単語情報読み込み
-		
-		// 単語情報出力
-		
-		System.out.println("上手");
+    public static void main(String[] args) {
 
-	}
+        // 引数チェック
+        if (!checkArgs(args)) {
+            return;
+        }
 
-	// 引数チェック
-	// 引数が1つ以外の場合、エラーメッセージを表示し、チェックエラーとする
-	private static boolean checkArgs(String[] args) {
+        // 単語情報ファイル存在チェック
+        if (!existDictionaryDataFile(args[0])) {
+            return;
+        }
 
-		// 単語情報ファイルが指定されているか確認する
-		if (args.length !=1) {
-			System.out.println("単語情報ファイルを指定して下さい。");
-			return false;
-		}
-		return true;
-	}
-	
-	// 単語情報ファイル存在チェック
-	// 単語情報ファイルが存在しない場合、チェックエラーとする
-	private static boolean existDictionaryDataFile(String dictionaryDataFilePath) {
+        // 単語情報読み込み
+        String dictionaryData = readDictionaryData(args[0]);
 
-		// ファイル情報取得
-		File dictionaryDataFile = new File(dictionaryDataFilePath);
-		
-		// 単語情報ファイルが存在するか確認する
-		if (!dictionaryDataFile.exists()) {
-			System.out.println("単語情報ファイルが存在しません");
-			System.out.println("指定ファイル：" + dictionaryDataFilePath);
-			return false;
-		}
-		return true;
-	}
-	
-	
+        // 単語情報出力
+        System.out.println(dictionaryData);
+    }
+
+    // 引数チェック
+    // 引数が1つ以外の場合、エラーメッセージを表示し、チェックエラーとする
+    private static boolean checkArgs(String[] args) {
+
+        // 単語情報ファイルが指定されているか確認する
+        if (args.length != 1) {
+            System.out.println("単語情報ファイルを指定して下さい。");
+            return false;
+        }
+        return true;
+    }
+
+    // 単語情報ファイル存在チェック
+    // 単語情報ファイルが存在しない場合、チェックエラーとする
+    private static boolean existDictionaryDataFile(String dictionaryDataFilePath) {
+
+        // ファイル情報取得
+        File dictionaryDataFile = new File(dictionaryDataFilePath);
+
+        // 単語情報ファイルが存在するか確認する
+        if (!dictionaryDataFile.exists()) {
+            System.out.println("単語情報ファイルが存在しません");
+            System.out.println("指定ファイル：" + dictionaryDataFilePath);
+            return false;
+        }
+        return true;
+    }
+
+    // 指定されたファイルパスのファイルを読み込み、内容の文字列を返却する
+    // ファイルの読み込みに失敗した場合は空の文字列を返却する
+    private static String readDictionaryData(String dictionaryDataFilePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(dictionaryDataFilePath,
+                StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            System.out.println("ファイルの読み込みに失敗しました:" + dictionaryDataFilePath);
+        }
+        return "";
+    }
 }
